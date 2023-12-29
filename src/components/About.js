146a356ead4e824e14e2a068f528773ff8cb2108
ViveK Poonia <vivekpoonia2112@gmail.com>
2023-12-29 14:35:36 +0530

    about us bg image

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,20 @@
 import "./About.css";
 import React from "react";
+import about_img from "../assets/IMG_8494.JPG";
 
 const About = () => {
-  return <div className="about">
-            <div className="about_pic">
-              <div className="a_pic"></div>
-            </div>
-            <div className="about_info">
-              <div className="">
-
-              </div>
-            </div>
-         </div>;
+  return (
+    <div className="about">
+      <div className="about_pic">
+        <div className="a_pic">
+          <img src={about_img} alt="img" />
+        </div>
+      </div>
+      <div className="about_info">
+        <div className=""></div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
