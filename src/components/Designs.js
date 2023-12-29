afd161d1a29da62bc19fc5ef416fbe5cf72c5388
Oct-o-Dev <Abhineetk213@gmail.com>
2023-12-29 14:02:34 +0530

    about us basic div

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -2,7 +2,26 @@ import "./Designs.css";
 import React from "react";
 
 const Design = () => {
-  return <div className="design">I am Design</div>;
+  return <div className="design">
+          <div className="d_box">   
+           <div className="box1"></div>
+           <div className="box1"></div>
+           <div className="box1"></div>
+           <div className="box1"></div>
+          </div> 
+          <div className="d_box">   
+           <div className="box1"></div>
+           <div className="box1"></div>
+           <div className="box1"></div>
+           <div className="box1"></div>
+          </div> 
+          <div className="d_box">   
+           <div className="box1"></div>
+           <div className="box1"></div>
+           <div className="box1"></div>
+           <div className="box1"></div>
+          </div> 
+        </div>
 };
 
 export default Design;
