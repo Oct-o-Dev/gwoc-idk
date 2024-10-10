1f1e7433662584225e91bc745ef9a5bf7b68e2c5
ViveK Poonia <vivekpoonia2112@gmail.com>
2024-10-10 12:44:20 +0530

    Reverting

--- src/components/Activities2.js
+++
@@ -1,8 +0,0 @@
-import "./Activities2.css";
-import React from "react";
-
-const Activities2 = () => {
-  return <div className="activities2">I am details of activities2</div>;
-};
-
-export default Activities2;
--- src/components/Contact.js
+++
@@ -1,8 +0,0 @@
-import "./Contact.css";
-import React from "react";
-
-const Contact = () => {
-  return <div className="contact">I am Contact</div>;
-};
-
-export default Contact;
